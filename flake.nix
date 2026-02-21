1388683ec2c0bebcb4f405d5f08861eda71e175a
SemicolonUnexpected <m.gracey1409@gmail.com>
2026-02-21 12:28:44 +0000

    eufs_msgs work

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   description = "EUFS workspace";
+
   inputs = {
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs";
   };
+
   outputs =
     {
       self,
@@ -12,22 +14,39 @@
     }:
     let
       system = "x86_64-linux";
+
       pkgs = import nixpkgs {
         inherit system;
+
         overlays = [ nix-ros-overlay.overlays.default ];
+
+        config = {
+          allowUnfree = true; # Often needed for ROS drivers
+          permittedInsecurePackages = [
+            "freeimage-3.18.0-unstable-2024-04-18"
+          ];
+        };
       };
+
       ros = pkgs.rosPackages.humble;
+
       eufsPackages = import ./packages.nix { inherit pkgs ros; };
     in
     {
       packages.${system} = eufsPackages // {
-        default = eufsPackages.eufs-sim;
+        default = eufsPackages.eufs-msgs; # eufs-sim;
       };
+
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
           eufsPackages.eufs-msgs
-          eufsPackages.eufs-sim
+          # eufsPackages.eufs-sim
         ];
       };
     };
+
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
 }
--- packages.nix
+++ packages.nix
@@ -39,125 +39,158 @@ let
     }
   ) { };
 
-  eufs-launcher = ros.callPackage (
-    {
-      buildRosPackage,
-      ament-cmake,
-      ament-cmake-python,
-      ...
-    }:
-    buildRosPackage {
-      pname = "eufs-launcher";
-      version = "0.0.0";
-      src = "${eufs-sim-src}/eufs_launcher";
-      buildType = "ament_python";
-      nativeBuildInputs = [
-        ament-cmake
-        ament-cmake-python
-      ];
-      propagatedBuildInputs = [ eufs-msgs ];
-    }
-  ) { };
+  # eufs-launcher = ros.callPackage (
+  #   {
+  #     buildRosPackage,
+  #     ament-cmake,
+  #     ament-cmake-python,
+  #     ...
+  #   }:
+  #   buildRosPackage {
+  #     pname = "eufs-launcher";
+  #     version = "0.0.0";
+  #     src = "${eufs-sim-src}/eufs_launcher";
+  #     buildType = "ament_python";
+  #     nativeBuildInputs = [
+  #       ament-cmake
+  #       ament-cmake-python
+  #     ];
+  #     propagatedBuildInputs = [ eufs-msgs ];
+  #   }
+  # ) { };
 
-  eufs-models = ros.callPackage (
-    { buildRosPackage, ament-cmake, ... }:
-    buildRosPackage {
-      pname = "eufs-models";
-      version = "0.0.0";
-      src = "${eufs-sim-src}/eufs_models";
-      nativeBuildInputs = [ ament-cmake ];
-      propagatedBuildInputs = [ eufs-msgs ];
-    }
-  ) { };
+  # eufs-models = ros.callPackage (
+  #   {
+  #     buildRosPackage,
+  #     ament-cmake,
+  #     yaml-cpp,
+  #     ...
+  #   }:
+  #   buildRosPackage {
+  #     pname = "eufs-models";
+  #     version = "0.0.0";
+  #     src = "${eufs-sim-src}/eufs_models";
+  #     nativeBuildInputs = [ ament-cmake ];
+  #     propagatedBuildInputs = [
+  #       eufs-msgs
+  #       yaml-cpp
+  #     ];
+  #   }
+  # ) { };
 
-  eufs-plugins = ros.callPackage (
-    { buildRosPackage, ament-cmake, ... }:
-    buildRosPackage {
-      pname = "eufs-plugins";
-      version = "0.0.0";
-      src = "${eufs-sim-src}/eufs_plugins";
-      nativeBuildInputs = [ ament-cmake ];
-      propagatedBuildInputs = [ eufs-msgs ];
-    }
-  ) { };
+  # eufs-plugins = ros.callPackage (
+  #   {
+  #     buildRosPackage,
+  #     ament-cmake,
+  #     gazebo-dev,
+  #     gazebo-ros,
+  #     nav-msgs,
+  #     tf2,
+  #     tf2-ros,
+  #     ackermann-msgs,
+  #     image-geometry,
+  #     yaml-cpp-vendor,
+  #     tf2-geometry-msgs,
+  #     ...
+  #   }:
+  #   buildRosPackage {
+  #     pname = "eufs-plugins";
+  #     version = "0.0.0";
+  #     src = "${eufs-sim-src}/eufs_plugins";
+  #     nativeBuildInputs = [ ament-cmake ];
+  #     propagatedBuildInputs = [
+  #       eufs-msgs
+  #       tf2
+  #       tf2-geometry-msgs
+  #       tf2-ros
+  #       nav-msgs
+  #       gazebo-dev
+  #       gazebo-ros
+  #       eufs-models
+  #       ackermann-msgs
+  #       image-geometry
+  #       yaml-cpp-vendor
+  #     ];
+  #   }
+  # ) { };
 
-  eufs-racecar = ros.callPackage (
-    {
-      buildRosPackage,
-      ament-cmake,
-      ament-cmake-python,
-      ...
-    }:
-    buildRosPackage {
-      pname = "eufs-racecar";
-      version = "0.0.0";
-      src = "${eufs-sim-src}/eufs_racecar";
-      buildType = "ament_python";
-      nativeBuildInputs = [
-        ament-cmake
-        ament-cmake-python
-      ];
-      propagatedBuildInputs = [ eufs-msgs ];
-    }
-  ) { };
+  # eufs-racecar = ros.callPackage (
+  #   {
+  #     buildRosPackage,
+  #     ament-cmake,
+  #     ament-cmake-python,
+  #     ...
+  #   }:
+  #   buildRosPackage {
+  #     pname = "eufs-racecar";
+  #     version = "0.0.0";
+  #     src = "${eufs-sim-src}/eufs_racecar";
+  #     buildType = "ament_python";
+  #     nativeBuildInputs = [
+  #       ament-cmake
+  #       ament-cmake-python
+  #     ];
+  #     propagatedBuildInputs = [ eufs-msgs ];
+  #   }
+  # ) { };
 
-  eufs-rqt = ros.callPackage (
-    {
-      buildRosPackage,
-      ament-cmake,
-      ament-cmake-python,
-      ...
-    }:
-    buildRosPackage {
-      pname = "eufs-rqt";
-      version = "0.0.0";
-      src = "${eufs-sim-src}/eufs_rqt";
-      buildType = "ament_python";
-      nativeBuildInputs = [
-        ament-cmake
-        ament-cmake-python
-      ];
-      propagatedBuildInputs = [ eufs-msgs ];
-    }
-  ) { };
+  # eufs-rqt = ros.callPackage (
+  #   {
+  #     buildRosPackage,
+  #     ament-cmake,
+  #     ament-cmake-python,
+  #     ...
+  #   }:
+  #   buildRosPackage {
+  #     pname = "eufs-rqt";
+  #     version = "0.0.0";
+  #     src = "${eufs-sim-src}/eufs_rqt";
+  #     buildType = "ament_python";
+  #     nativeBuildInputs = [
+  #       ament-cmake
+  #       ament-cmake-python
+  #     ];
+  #     propagatedBuildInputs = [ eufs-msgs ];
+  #   }
+  # ) { };
 
-  eufs-sensors = ros.callPackage (
-    { buildRosPackage, ament-cmake, ... }:
-    buildRosPackage {
-      pname = "eufs-sensors";
-      version = "0.0.0";
-      src = "${eufs-sim-src}/eufs_sensors";
-      nativeBuildInputs = [ ament-cmake ];
-      propagatedBuildInputs = [ eufs-msgs ];
-    }
-  ) { };
+  # eufs-sensors = ros.callPackage (
+  #   { buildRosPackage, ament-cmake, ... }:
+  #   buildRosPackage {
+  #     pname = "eufs-sensors";
+  #     version = "0.0.0";
+  #     src = "${eufs-sim-src}/eufs_sensors";
+  #     nativeBuildInputs = [ ament-cmake ];
+  #     propagatedBuildInputs = [ eufs-msgs ];
+  #   }
+  # ) { };
 
-  eufs-tracks = ros.callPackage (
-    { buildRosPackage, ament-cmake, ... }:
-    buildRosPackage {
-      pname = "eufs-tracks";
-      version = "0.0.0";
-      src = "${eufs-sim-src}/eufs_tracks";
-      buildType = "ament_python";
-      nativeBuildInputs = [ ament-cmake ];
-      propagatedBuildInputs = [ eufs-msgs ];
-    }
-  ) { };
+  # eufs-tracks = ros.callPackage (
+  #   { buildRosPackage, ament-cmake, ... }:
+  #   buildRosPackage {
+  #     pname = "eufs-tracks";
+  #     version = "0.0.0";
+  #     src = "${eufs-sim-src}/eufs_tracks";
+  #     buildType = "ament_python";
+  #     nativeBuildInputs = [ ament-cmake ];
+  #     propagatedBuildInputs = [ eufs-msgs ];
+  #   }
+  # ) { };
 
-  eufs-sim = pkgs.symlinkJoin {
-    name = "eufs-sim";
-    paths = [
-      eufs-launcher
-      eufs-models
-      eufs-plugins
-      eufs-racecar
-      eufs-rqt
-      eufs-sensors
-      eufs-tracks
-    ];
-  };
+  # eufs-sim = pkgs.symlinkJoin {
+  #   name = "eufs-sim";
+  #   paths = [
+  #     eufs-launcher
+  #     eufs-models
+  #     eufs-plugins
+  #     eufs-racecar
+  #     eufs-rqt
+  #     eufs-sensors
+  #     eufs-tracks
+  #   ];
+  # };
 
 in
 {
-  inherit eufs-msgs eufs-sim;
+  inherit eufs-msgs; # eufs-sim;
 }
